2ad75fa3ce0d5a9bb3c691bb06751a74676f422b
Benjamin Barbe <benjamin@pandasuite.com>
2021-06-02 14:45:06 +0200

    Purge only in production

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 const bridgeConfig = require('pandasuite-bridge/tailwind.config');
 
-bridgeConfig.purge.content.push('./node_modules/pandasuite-bridge-react/src/**/*.{js,jsx,ts,tsx}');
+bridgeConfig.purge.push('./node_modules/pandasuite-bridge-react/src/**/*.{js,jsx,ts,tsx}');
 
 module.exports = bridgeConfig;
