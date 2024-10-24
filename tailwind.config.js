15a942a47ed2e1b336b2aa46e6836d41f070c4af
Benjamin Barbe <benjamin@pandasuite.com>
2024-10-24 15:47:43 +0200

    Upgrade tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 const bridgeConfig = require('pandasuite-bridge/tailwind.config');
 
-bridgeConfig.purge.push('./node_modules/pandasuite-bridge-react/src/**/*.{js,jsx,ts,tsx}');
+bridgeConfig.content.push(
+  './node_modules/pandasuite-bridge-react/src/**/*.{js,jsx,ts,tsx}',
+);
 
 module.exports = bridgeConfig;
